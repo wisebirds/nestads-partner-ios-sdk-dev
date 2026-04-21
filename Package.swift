154d972bc36e53fa8880a6fdf4a859f40a38831f
Bitbucket Pipeline <ci@wisebirds.com>
2026-04-21 18:20:00 +0900

    release v1.0.0-dev

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.9
+import PackageDescription
+let package = Package(
+  name: "NestAdsPartnerSDK",
+  platforms: [
+    .iOS(.v13)
+  ],
+  products: [
+    .library(
+      name: "NestAdsPartnerSDK",
+      targets: ["NestAdsPartnerSDKWrapper"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://bitbucket.org/wisebirds/nestads-sdk-ios-package", exact: "2.12.0-dev"),
+    .package(url: "https://github.com/covigroup/COVI-iOS-SDK.git", from: "1.2.1")
+  ],
+  targets: [
+    .binaryTarget(
+      name: "NestAdsPartnerSDK",
+      url: "https://github.com/wisebirds/nestads-partner-ios-sdk-dev/releases/download/1.0.0-dev/NestAdsPartnerSDK.xcframework.zip",
+      checksum: "bd19d2888b7ddf12408f55f7ae6216d1d6bddd87d7e040d85c4e2f16e55ab921"
+    ),
+    .target(
+      name: "NestAdsPartnerSDKWrapper",
+      dependencies: [
+        "NestAdsPartnerSDK",
+        .product(name: "NestAdsSDK", package: "nestads-sdk-ios-package"),
+        .product(name: "COVI-iOS-SDK", package: "COVI-iOS-SDK")
+      ],
+      path: "Sources/Wrapper"
+    )
+  ]
+)
